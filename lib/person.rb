7b04c25847652ccde577ef73c98401ce02cfb3d4
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-11-04 14:48:01 -0400

    commit tree ee84932a17258169429d29f2c4ab9dd3067d70b9

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def talk
+    puts "Hello World!"
+  end
+
+  def intro
+    puts "my name is Mike"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+# mike = Person.new
+# puts mike.talk
+# puts mike.intro
